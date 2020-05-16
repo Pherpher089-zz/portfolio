07e9bedb1cc8a533d103ceb9a68a5191914c8c6b
Pherpher089 <Christutor089@gmail.com>
2020-05-16 10:34:13 -0700

    Finished styleing the footer

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import "./Footer.scss";
 function Footer(props) {
 	return (
-		<div>
-			<div>Contact Info</div>
-			<div>
+		<div className="Footer">
+			<address>
+				<email>email: christutor089@gmail.com</email>
+				<phone>phone: (408)375-5016 {"\n"}</phone>
+				<a href="https://github.com/Pherpher089">
+					GitHub: github.com/Pherpher089
+				</a>
+				<a href="https://www.linkedin.com/in/christopher-tutor-68a3b796/">
+					LinkedIn: www.linkedin.com/in/chris-tutor
+				</a>
+			</address>
+			<div className="Footer-Nav">
 				<a>Contact Me</a>
 				<a>About Me</a>
 				<a>Projects</a>
