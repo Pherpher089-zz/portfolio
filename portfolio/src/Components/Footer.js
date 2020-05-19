2e91350c4adfdb78c8c0a546c6056095dfbd2afe
Pherpher089 <Christutor089@gmail.com>
2020-05-18 19:55:37 -0700

    Made some sizing adjustments to a few different widths to remove the lower scroll bar

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -4,13 +4,17 @@ function Footer(props) {
 	return (
 		<div className="Footer">
 			<address>
-				<email>email: christutor089@gmail.com</email>
-				<phone>phone: (408)375-5016 {"\n"}</phone>
+				<div>email</div>
+				<email>christutor089@gmail.com</email>
+				<div>Phone</div>
+				<phone>(408)375-5016 {"\n"}</phone>
+				<div>GitHub</div>
 				<a href="https://github.com/Pherpher089">
-					<p>GitHub: </p> github.com/Pherpher089
+					github.com/Pherpher089
 				</a>
+				<div>LinkedIn</div>
 				<a href="https://www.linkedin.com/in/christopher-tutor-68a3b796/">
-					<p>LinkedIn: </p> www.linkedin.com/in/chris-tutor
+					www.linkedin.com/in/chris-tutor
 				</a>
 			</address>
 			<div className="Footer-Nav">
